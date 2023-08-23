d4fb0b642dfacf402da09fc76bd8a5b1c2e15bb5
Jaei <dobbykim0320@gmail.com>
2023-08-23 15:18:04 +0900

    [section5] type-aliases 수강완료

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,17 @@
+interface Person{
+  name:string;
+  age: number;
+}
+
+// type Person ={
+//   name:string;
+//   age: number;
+// }
+
+var seho : Person={
+  name:'세호',
+  age: 27
+}
+
+type MyString = string;
+var str: MyString='hello'
