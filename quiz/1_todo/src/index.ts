8a19eee858bd8d3e4abad01950f623bd0e8907f9
Jaei <dobbykim0320@gmail.com>
2023-08-23 13:59:28 +0900

    [section4] 할 일 관리 애플리케이션

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,23 @@
-let todoItems;
+// type Todo={
+//   id: number;
+//   title:string;
+//   done: boolean;
+// }
+
+interface Todo{
+  id: number;
+    title:string;
+    done: boolean;
+}
+
+let todoItems:{
+  id: number;
+  title: string;
+  done: boolean;
+}[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems():Todo[]{
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,20 +27,20 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos():object[] {          
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo:Todo):void{
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index:number) {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index:number, todo:{id: number, title: string, done: boolean}) {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -39,12 +55,24 @@ function showCompleted() {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+function addTwoTodoItems():void {
+  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.\
+  const item1={
+    id:4,
+    title:'아이템4',
+    done: false,
+  }
+  addTodo(item1);
+  addTodo({
+    id:5,
+    title:'아이템5',
+    done: false,
+  });
+  
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log() :void{
   console.log(todoItems);
 }
 
